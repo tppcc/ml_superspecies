a048987ba9a7c1ffa353697d17df8e29c93eb5ad
tppcc <pak.yuen.tai@outlook.com>
2023-12-14 14:02:57 +0100

    regular update and created baseline directory

--- nmf_baseline/nmf.py
+++ nmf_baseline/nmf.py
@@ -10,30 +10,39 @@ import os
 #                           the projection matrix to latent space is termed H instead of W, while the
 #                           latent represented data matrix is termed W, such that
 #                           argmin|| V(N,C) - H(N,R) * W(C,R)^T || is performed with beta divergence
-#                           minimisation
+#                           minimisation routine
 ################
 
 
 class nmf_object:
-    def __init__(self, V, rank=None, inheretence=False, H_init=None, W_init=None):
-        self.V = V
+    def __init__(self, rank=None, inheretence=False, H_init=None, W_init=None, kernel_size=(1,1,1)):
         self.inheretence = inheretence
         self.W = W_init
         self.H = H_init
         self.inheretence_check()
         self.rank = rank
+        self.dimensionality_check()
+        self.kernel_size=kernel_size
 
     def inheretence_check(self):
         if self.inheretence == False:
-            if self.W != None & self.H != None:
+            if (self.W != None) & (self.H != None):
                 raise Exception("W_init & H_init exist when inheretence is set to False!")
+
+    def dimensionality_check(self):
+        if self.H.shape[1] != self.rank:
+            raise Exception("H_init shape is incorrect, dimension 1 should match number of rank")
+        if self.W.shape[1] != self.rank:
+            raise Exception("W_init shape is incorrect, dimension 1 should match number of rank")
+
 
     def nmf(self):
         #initialise NMF class
         if self.inheretence == True:
+            # can be changed to NMF3D H (N,R, x, y, z)
             self.instance = nmf.nmf.NMF(H=self.H, W=self.W, rank=self.rank)
         elif self.inheretence == False:
             self.instance = nmf.nmf.NMF(rank=self.rank)
 
-
-instance = nmf.nmf.NMF()
+    def trainer(self, V, beta=1, tol=0.0001, max_iter=200, verbose=False, alpha=0, l1_ratio=0):
+        self.instance.fit(V, beta=beta, tol=tol, max_iter=max_iter, verbose=verbose, alpha=alpha, l1_ratio=l1_ratio)
