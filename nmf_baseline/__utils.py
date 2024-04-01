e9cfce7ad46eeb7eebf15efb839b6de3993a84b7
tppcc <pak.yuen.tai@outlook.com>
2024-04-01 21:11:26 +0200

    Updated NonNegTrainer

--- nmf_baseline/__utils.py
+++ nmf_baseline/__utils.py
@@ -11,7 +11,29 @@ class utils:
         Args:
             y (ndarray): Array of the True state
             y_predict (ndarray); Array of the Predicted state
+        Return:
+            rmse (float): computed RMSE for the current array
         """
 
         assert y.shape == y_predict.shape, "The input y and y_predict is not the same"
-        return np.sqrt((1 / y.size) * np.sum(np.square(y_predict - y)))
+        return np.sum(np.sqrt((1 / y.size) * np.sum(np.square(y_predict - y))))
+
+    def __rrmse(self, y, y_predict, rmse=None):
+        r"""Calculates the Relative (Normalised) Root Mean Square Error
+        Args:
+            y (ndarray): Array of the True state
+            y_predict (ndarray): Array of the Predicted state
+            rmse (float, optional): computed RMSE, if this is provided, will skip the full computation
+                                    and fall back to rmse / np.sum(y) to save computation resources
+        Return:
+            rrmse (float): computed Relative RMSE for the current array
+        """
+
+        assert y.shape == y_predict.shape, "The input y and y_predict is not the same"
+        if rmse != None:
+            assert type(rmse) == float, "incorrect data type for rmse (float)"
+            return rmse / np.sum(y)
+        elif rmse == None:
+            return np.sum(np.sqrt((1 / y.size) * np.sum(np.square(y_predict - y))) / np.sum(y))
+        else:
+            raise Exception("incorrect data type for rmse (float)")
